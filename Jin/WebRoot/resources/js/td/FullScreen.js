8cd9718914977a228d7ab28da8114c4bd8c631fc
brucestifler <brucestifler@brucestifler-virtual-machine>
2018-03-20 15:04:42 +0800

    全屏曲线和数据为空bug修正

--- Jin/WebRoot/resources/js/td/FullScreen.js
+++ Jin/WebRoot/resources/js/td/FullScreen.js
@@ -1,20 +1,16 @@
 /**
  * 
  */
-var da;
-var data1;
-var dat;
-var num = 0;
-var num0 = 0;
-var num1 = 0;
-var num2 = 0;
-var num3 = 0;
-var on1=new Array();
-var on=new Array();
-var warn=new Array();
-var wait=new Array();
-var off=new Array();
+var data={};
 var iname=0;
+var maxele;
+var maxvol;
+var minele;
+var minvol;
+var dd;
+var z=0;
+var jj=0;
+var ii;
 $(function(){
 	$.ajax({  
 	      type : "post",  
@@ -48,34 +44,38 @@ $(function() {
 		};
 		//获得消息事件
 		socket.onmessage = function(msg) {
-			dat = msg.data;
+			dd = msg.data;
 			for(var j = 0;j < 1;j++){
 				for(var i = 0;i < dd.length;i+=53){
-						if(hq == parseInt(dd.substring(4+i, 8+i),16)&&(dd.substring(8+i, 12+i))!="0000"){
-						var defineStr = "var ele"+iname.toString();
-						var mach = parseInt(dd.substring(4+i, 8+i),16);
-						var weld = dd.substring(8+i, 12+i);
-						var xx = dd.substring(12+i, 16+i);
-						ele[jj] = parseInt(xx,16);
-						vol[jj] = parseInt(dd.substring(16+i, 20+i),16);
-						var dati = dd.substring(20+i, 39+i);
-						var val = Date.parse(dati);
-						time1[jj] = val;
-						maxele = parseInt(dd.substring(41+i, 44+i));
-						minele = parseInt(dd.substring(44+i, 47+i));
-						maxvol = parseInt(dd.substring(47+i, 50+i));
-						minvol = parseInt(dd.substring(50+i, 53+i));
-						document.getElementById("macname").value=hq;
-						for(var k=0;k<namex.length;k++){
-							if(namex[k].fwelder_no==dd.substring(8+i, 12+i)){
-								document.getElementById("welname").value=namex[k].fname;
+						if((dd.substring(8+i, 12+i))!="0000"){
+							if($("#div"+dd.substring(4+i, 8+i)+"").length<=0){
+								var str = "<div style='float:left;'>"+
+								"<div id='div"+dd.substring(4+i, 8+i)+"' style='float:left;'/><div/>&nbsp;"+
+								"<div id='div1"+dd.substring(4+i, 8+i)+"' style='float:left;'/><div/>&nbsp;"+
+								"<div/>";
+								$("#body").append(str);
+								if(jj%3==0){
+								ii = i;
+							    data['ele'+dd.substring(4+ii, 8+ii)] = [];
+							    data['vol'+dd.substring(4+ii, 8+ii)] = [];
+							    data['time'+dd.substring(4+ii, 8+ii)] = [];
+								}
+							    data['ele'+dd.substring(4+ii, 8+ii)].push(parseInt(dd.substring(12+i, 16+i)),16);
+							    data['vol'+dd.substring(4+ii, 8+ii)].push(parseInt(dd.substring(16+i, 20+i),16));
+							    data['time'+dd.substring(4+ii, 8+ii)].push(Date.parse(dd.substring(20+i, 39+i)));
+								maxele = parseInt(dd.substring(41+i, 44+i));
+								minele = parseInt(dd.substring(44+i, 47+i));
+								maxvol = parseInt(dd.substring(47+i, 50+i));
+								minvol = parseInt(dd.substring(50+i, 53+i));
+								jj++;
+								if(jj<=1){
+									curve();
+									}
+							}else{
+							    data['ele'+dd.substring(4+ii, 8+ii)].push(parseInt(dd.substring(12+i, 16+i)),16);
+							    data['vol'+dd.substring(4+ii, 8+ii)].push(parseInt(dd.substring(16+i, 20+i),16));
+							    data['time'+dd.substring(4+ii, 8+ii)].push(Date.parse(dd.substring(20+i, 39+i)));
 							}
-						}
-						jj++;
-						if(jj<=1){
-							curve(); 
-							curve1();
-						}
 						}	
 				}
 				if(jj%3==1){
@@ -94,9 +94,6 @@ $(function() {
 					jj++;
 				}
 				}
-			var str = "<div id='div"+index+"' style='width:270px;heigth:240px;float:left;'>"+"<div/>";
-			$("#body").append(str);
-
 		};
 		//关闭事件
 		socket.onclose = function() {
@@ -116,3 +113,244 @@ $(function() {
 		socket.close();
 	});
 });
+
+function curve(){
+		Highcharts.setOptions({
+		    global: {
+		        useUTC: false
+		    }
+		});
+		function activeLastPointToolip(chart) {
+		    var points = chart.series[0].points;
+/*  		    chart.tooltip.refresh(points[points.length -1]);
+		    chart.tooltip.refresh(points1[points1.length -1]);*/
+		  	chart.yAxis[0].addPlotLine({ //在y轴上增加 
+		  		value:maxele, //在值为2的地方 
+		  		width:2, //标示线的宽度为2px 
+		  		color: 'red', //标示线的颜色 
+		  	    dashStyle:'longdashdot',
+		  		id: 'plot-line-1', //标示线的id，在删除该标示线的时候需要该id标示 });
+	          label:{
+		            text:'最高电流',     //标签的内容
+		            align:'center',                //标签的水平位置，水平居左,默认是水平居中center
+		            x:10                         //标签相对于被定位的位置水平偏移的像素，重新定位，水平居左10px
+		        }
+		  	})
+		  	chart.yAxis[0].addPlotLine({ //在y轴上增加 
+		  		value:minele, //在值为2的地方 
+		  		width:2, //标示线的宽度为2px 
+		  		color: 'red', //标示线的颜色 
+		  	    dashStyle:'longdashdot',
+		  		id: 'plot-line-1', //标示线的id，在删除该标示线的时候需要该id标示 });
+	          label:{
+		            text:'最低电流',     //标签的内容
+		            align:'center',                //标签的水平位置，水平居左,默认是水平居中center
+		            x:10                     //标签相对于被定位的位置水平偏移的像素，重新定位，水平居左10px
+		        }
+		  	})	  	
+		}
+		$('#div'+dd.substring(4+i, 8+i)+'').highcharts({
+		    chart: {
+		        type: 'spline',
+		        animation: false, // don't animate in old IE
+		        marginRight: 70,
+		        events: {
+		            load: function () {
+		                // set up the updating of the chart each second
+		                var series = this.series[0],
+		                    chart = this;
+		                	window.setInterval(function () {
+		                    /*var x = (new Date()).getTime()+t,*/ // current time
+		                  var x = data['time'+dd.substring(4+ii, 8+ii)][z],
+		                       y = data['ele'+dd.substring(4+ii, 8+ii)][z];
+		                    z++;
+		                    series.addPoint([x, y], true, true);
+		                    activeLastPointToolip(chart);
+		                }, 1000);
+		            }
+		        }
+		    },
+		    title: {
+		        text: '电压电流实时监测'
+		    },
+		    xAxis: {
+		        type: 'datetime',
+		        tickPixelInterval: 150,
+		        lineColor:'#FFFFFF',
+		        tickWidth:0,
+  		    labels:{
+  		    	enabled:false
+  		    }
+		    },
+		    yAxis: [{
+            max:280, // 定义Y轴 最大值  
+            min:0, // 定义最小值  
+            minPadding: 0.2,   
+            maxPadding: 0.2,  
+            tickInterval:40,
+            color:'#A020F0',
+		        title: {
+		            text: '电流',
+	                style: {  
+	                    color: '#A020F0'  
+	                }  
+		        }
+		    }],
+		    tooltip: {
+		        formatter: function () {
+		            return '<b>' + this.series.name + '</b><br/>' +
+		                Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+		                Highcharts.numberFormat(this.y, 2);
+		        },
+		    },
+		    legend: {
+		        enabled: false
+		    },
+		    exporting: {
+		        enabled: false
+		    },
+		    credits:{
+		      enabled:false // 禁用版权信息
+		    },
+		    series: [{
+		    	color:'#A020F0',
+		        name: '电流',
+		        
+		        data: (function () {
+		            // generate an array of random data
+		            var data = [],
+		                time = (new Date()).getTime(),
+		                /*time = new Date(Date.parse("0000-00-00 00:00:00")),*/
+		                i;
+		            for (i = -9; i <= 0; i += 1) {
+		                data.push({
+		                    x: data['time'+dd.substring(4+ii, 8+ii)][0]-1000+i*1000,
+		                	/*x: time + i*1000,*/
+		                    y: 0
+		                });
+		            }
+		            return data;
+		        }())
+		    }]
+		}, function(c) {
+		    activeLastPointToolip(c)
+		});
+
+}
+
+function curve1(){
+		Highcharts.setOptions({
+		    global: {
+		        useUTC: false
+		    }
+		});
+		function activeLastPointToolip(chart) {
+		    var points = chart.series[0].points;
+/*  		    chart.tooltip.refresh(points[points.length -1]);
+		    chart.tooltip.refresh(points1[points1.length -1]);*/
+		  	chart.yAxis[0].addPlotLine({ //在y轴上增加 
+		  		value:maxvol, //在值为2的地方 
+		  		width:2, //标示线的宽度为2px 
+		  		color: 'black', //标示线的颜色 
+		  	    dashStyle:'longdashdot',
+		  		id: 'plot-line-1', //标示线的id，在删除该标示线的时候需要该id标示 });
+	          label:{
+		            text:'最高电压',     //标签的内容
+		            align:'center',                //标签的水平位置，水平居左,默认是水平居中center
+		            x:10  
+		        }
+		  	})
+		  	chart.yAxis[0].addPlotLine({ //在y轴上增加 
+		  		value:minvol, //在值为2的地方 
+		  		width:2, //标示线的宽度为2px 
+		  		color: 'black', //标示线的颜色 
+		  	    dashStyle:'longdashdot',
+		  		id: 'plot-line-1', //标示线的id，在删除该标示线的时候需要该id标示 });
+	          label:{
+		            text:'最低电压',     //标签的内容
+		            align:'center',                //标签的水平位置，水平居左,默认是水平居中center
+		            x:10                         //标签相对于被定位的位置水平偏移的像素，重新定位，水平居左10px
+		        }
+		  	})
+		  	  		  	
+		}
+		$('#div1'+dd.substring(4+i, 8+i)+'').highcharts({
+		    chart: {
+		        type: 'spline',
+		        animation: false, // don't animate in old IE
+		        marginRight: 70,
+		        events: {
+		            load: function () {
+		                // set up the updating of the chart each second
+		                var series = this.series[0],
+		                    chart = this;
+		                	window.setInterval(function () {
+		                    /*var x = (new Date()).getTime()+t,*/ // current time
+			                  var x = data['time'+dd.substring(4+ii, 8+ii)][z],
+		                       y = data['vol'+dd.substring(4+ii, 8+ii)][z];
+		                    zz++;
+		                    series.addPoint([x, y], true, true);
+		                    activeLastPointToolip(chart);
+		                }, 1000);
+		            }
+		        }
+		    },
+		    title: {
+		        text: false
+		    },
+		    xAxis: {
+		        type: 'datetime',
+		        tickPixelInterval: 150
+		    },
+		    yAxis: [{
+            max:105, // 定义Y轴 最大值  
+            min:0, // 定义最小值  
+            minPadding: 0.2,   
+            maxPadding: 0.2,  
+            tickInterval:15,
+            color:'#87CEFA',
+		    	title: {
+		            text: '电压',
+	                style: {  
+	                    color: '#87CEFA'  
+	                }  
+		        },
+		    }],
+		    tooltip: {
+		        formatter: function () {
+		            return '<b>' + this.series.name + '</b><br/>' +
+		                Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+		                Highcharts.numberFormat(this.y, 2);
+		        },
+		    },
+		    legend: {
+		        enabled: false
+		    },
+		    exporting: {
+		        enabled: false
+		    },
+		    credits:{
+		      enabled:false // 禁用版权信息
+		    },
+		    series: [{
+		        name: '电压',
+		        data: (function () {
+		            // generate an array of random data
+		            var data = [],
+		                time = (new Date()).getTime(),
+		                i;
+		            for (i = -9; i <= 0; i += 1) {
+		                data.push({
+		                    x: data['time'+dd.substring(4+ii, 8+ii)][0]-1000+i*1000,
+		                    /*x: time + i*1000,*/
+		                    y: 0
+		                });
+		            }
+		            return data;
+		        }()),
+		      
+		    }]
+		}, function(c) {
+		    activeLastPointToolip(c)
+		});
+}
