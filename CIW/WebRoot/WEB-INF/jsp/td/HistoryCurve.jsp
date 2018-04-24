899e1303f8099303df11c0f121bb5834d623efa5
gpyf16 <815137651@qq.com>
2018-04-24 09:58:58 +0800

    历史曲线

--- CIW/WebRoot/WEB-INF/jsp/td/HistoryCurve.jsp
+++ CIW/WebRoot/WEB-INF/jsp/td/HistoryCurve.jsp
@@ -29,6 +29,7 @@ String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.
 	<script type="text/javascript" src="resources/js/easyui-lang-zh_CN.js"></script>
 	<script type="text/javascript" src="resources/js/easyui-extend-check.js"></script>
 	<script type="text/javascript" src="resources/js/highcharts.js"></script>
+	<script type="text/javascript" src="resources/js/echarts.js"></script>
 	<script type="text/javascript" src="resources/js/getTimeADay.js"></script>
 	<script type="text/javascript" src="resources/js/exporting.js"></script>
 	<script type="text/javascript" src="resources/js/map.js"></script>
@@ -52,20 +53,15 @@ String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.
 				<a href="javascript:serachCompanyOverproof();" class="easyui-linkbutton" iconCls="icon-select" >搜索</a>
 			</div>
 		</div>
-		<div id="dgtb" style="width:100%;height:50%;">
+		<div id="dgtb" style="width:100%;height:100%;">
 			<table id="dg" style="table-layout:fixed;width:100%;"></table>
-<!-- 			<a class="easyui-linkbutton"  href="javascript:addtime()" iconCls="icon-add">加速</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -->
-<!-- 			<a class="easyui-linkbutton" href="javascript:reducetime()" iconCls="icon-remove">减速</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -->
-<!-- 			<a class="easyui-linkbutton"  href="javascript:starttime()" iconCls="icon-play">开始</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -->
-<!-- 			<a class="easyui-linkbutton" href="javascript:stoptime()" iconCls="icon-pause">暂停</a> -->
-			<a style="float: right">温馨提示：鼠标左键选中局部曲线可进行放大；‘ctrl’+鼠标左键可进行曲线平移。</a>
+			<div id="body1" style="position:absolute;top:60%;width:100%;height:20%;"></div>
+			<div id="body2" style="position:absolute;top:82%;width:100%;height:20%"></div>
 		</div>
-		<div id="load" style="width:100%;height:50%;"></div>
-		<div id="body1" style="position:absolute;top:63%;width:100%;height:20%"></div>
-		<div id="body2" style="position:absolute;top:81.5%;width:100%;height:20%"></div>
+		<div id="load" style="width:100%;height:40%;"></div>
 	</div>
 	<style type="text/css">
-    #load{ display: none; position: absolute; top: 63%; left: 0%; width: 100%; height: 37%; background-color: #555753; z-index:1001; -moz-opacity: 0.4; opacity:.40; filter: alpha(opacity=70);}
+    #load{ display: none; position: absolute; left:0; top:60%;width: 100%; height: 40%; background-color: #555753; z-index:1001; -moz-opacity: 0.4; opacity:.40; filter: alpha(opacity=70);}
 	#show{display: none; position: absolute; top: 80%; left: 45%; width: 10%; height: 5%; padding: 8px; border: 8px solid #E8E9F7; background-color: white; z-index:1002; overflow: auto;}
 	</style>
 </body>
--- CIW/WebRoot/resources/js/td/historycurves.js
+++ CIW/WebRoot/resources/js/td/historycurves.js
@@ -18,7 +18,7 @@ function Junction(){
 	setParam();
 	$("#dg").datagrid( {
 		fitColumns : true,				
-		height : $("#dgtb").height(),
+		height : $("#dgtb").height()/2,
 		width : $("#dgtb").width(),
 		idField : 'id',
 		pageSize : 10,
@@ -109,225 +109,180 @@ function Junction(){
             }
         },
         onClickRow: function(index,row){
-        	$('#body1').html("");
-			$('#body2').html("");
-			document.getElementById("load").style.display="block";
-			var sh = '<div id="show" style="width:150px;" align="center"><img src="resources/images/load1.gif"/>数据加载中，请稍候...</div>';
-			$("#body").append(sh);
-			document.getElementById("show").style.display="block";
-			chartStr = "";
-			setParam();
-			$.ajax({
-				   type: "post", 
-				   url: "rep/historyCurve"+chartStr+"&fid="+row.weldedJunctionno+"&mach="+row.machid+"&welderid="+$("#welderid").val(),
-				   dataType: "json",
-				   data: {},
-				   success: function (result) {
-				      if (result) {
-				    	  var date = eval(result.rows);
-				    	  if(date.length==0){
-				    		  document.getElementById("load").style.display ='none';
-				    		  document.getElementById("show").style.display ='none';
-				    		  alert("该时间内未查询到相关数据")
-				    	  }else{
-					    	  for(var i=0;i<date.length;i++){
-					    		  ele.push(date[i].ele);
-					    		  vol.push(date[i].vol);
-					    		  time1[i] = Date.parse(date[i].time);
-					    	  }
-					    	  eleChart();
-					    	  volChart();
-				    		  document.getElementById("load").style.display ='none';
-				    		  document.getElementById("show").style.display ='none';
-				    	  }
-				      }
-				   },
-				   error: function () {
-				      alert('error');
-				   }
-				});
+        	loadChart(row);
+        },
+        onSelect : function(index,row){
+        	loadChart(row);
         }
+        
 	});
 }
 
+function loadChart(row){
+	$('#body1').html("");
+	$('#body2').html("");
+	document.getElementById("load").style.display="block";
+	var sh = '<div id="show" style="width:150px;" align="center"><img src="resources/images/load1.gif"/>数据加载中，请稍候...</div>';
+	$("#body").append(sh);
+	document.getElementById("show").style.display="block";
+	chartStr = "";
+	setParam();
+	$.ajax({
+		   type: "post", 
+		   url: "rep/historyCurve"+chartStr+"&fid="+row.weldedJunctionno+"&mach="+row.machid+"&welderid="+$("#welderid").val(),
+		   dataType: "json",
+		   data: {},
+		   success: function (result) {
+		      if (result) {
+		    	  var date = eval(result.rows);
+		    	  if(date.length==0){
+		    		  document.getElementById("load").style.display ='none';
+		    		  document.getElementById("show").style.display ='none';
+		    		  alert("该时间内未查询到相关数据")
+		    	  }else{
+			    	  for(var i=0;i<date.length;i++){
+			    		  ele.push(date[i].ele);
+			    		  vol.push(date[i].vol);
+			    		  time1[i] = date[i].time;
+			    	  }
+			    	  eleChart();
+			    	  volChart();
+		    		  document.getElementById("load").style.display ='none';
+		    		  document.getElementById("show").style.display ='none';
+		    	  }
+		      }
+		   },
+		   error: function () {
+		      alert('error');
+		   }
+		});
+}
 
 function eleChart(){
-	Highcharts.setOptions({
-	    global: {
-	        useUTC: false
-	    },
-        lang: {
-            resetZoom: '重置',
-            resetZoomTitle: '重置缩放比例'
-        }
-	});
-	$('#body1').highcharts({
-	    title: {
-	        text: '电流电压历史回溯'
-	    },
-	    chart: {
-            panning: true,
-            panKey: 'ctrl',
-	    	zoomType: 'x',
-	        type: 'spline',
-	        animation: false, // don't animate in old IE
-	        marginRight: 70
-	    },
-	    plotOptions: {
-            line: {
-                dataLabels: {
-                    enabled: true
+    var myChart = echarts.init(document.getElementById('body1'));
+    var option = {
+        title : {
+            text : '电流'
+        },
+        tooltip : {
+            trigger : 'axis',
+        },
+        toolbox : {
+            show : true,
+            feature : {
+                mark : {
+                    show : false
                 },
-                enableMouseTracking: false
-            },
-	    	series:{
-	    		turboThreshold:0
-	    	}
+                dataView : {
+                    show : false,
+                    readOnly : false
+                },
+                restore : {
+                    show : false
+                }
+            }
         },
-	    title: {
-	        text: false
-	    },
-        mapNavigation: {
-            enabled: true,
-            enableButtons: false
+        dataZoom : {//缩放
+            show : true,
+            start : 0
         },
-	    xAxis: {
-	        type: 'datetime',
-	        tickPixelInterval: 150
-	    },
-	    yAxis: [{
-            max:650, // 定义Y轴 最大值  
-            min:0, // 定义最小值  
-            minPadding: 0.2,   
-            maxPadding: 0.2,  
-            tickInterval:130,
-            color:'#A020F0',
-	        title: {
-	            text: '电流',
-                style: {  
-                    color: '#A020F0'  
-                }  
-	        }
-	    }],
-	    tooltip: {
-	        formatter: function () {
-	            return '<b>' + this.series.name + '</b><br/>' +
-	                Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
-	                Highcharts.numberFormat(this.y, 2);
-	        },
-	    },
-	    legend: {
-	        enabled: false
-	    },
-	    exporting: {
-	        enabled: false
-	    },
-	    credits:{
-	      enabled:false // 禁用版权信息
-	    },
-	    series: [{
-	    	color:'#A020F0',
-	        name: '电流',
-	        data: (function () {
-	            var data = [];
-	            for (i = 0; i < time1.length; i += 1) {
-	                data.push({
-	                    x: time1[i],
-	                    y: ele[i]
-	                });
-	            }
-	            return data;
-	        }()),
-	      
-	    }]
-	});
-
+        grid : {
+            y2 : $("#body1").height()//图表高度
+        },
+        xAxis : [ {
+			type:'category',
+			data: time1
+        } ],
+        yAxis : [ {
+            type : 'value',
+            max : 650,
+            min : 0
+        } ],
+        series : [ {
+            symbolSize : 5,//气泡大小
+      		name : '电流',
+      		type :'line',//折线图
+      		data : ele,
+      		itemStyle: {
+      	        normal: {
+      	            color: "#A020F0",
+      	            lineStyle: {
+      	                color: "#A020F0"
+      	            }
+      	        }
+      	    },
+        } ]
+    };
+    myChart.setOption(option);
 }
 
 function volChart(){
-	Highcharts.setOptions({
-	    global: {
-	        useUTC: false
-	    },
-        lang: {
-            resetZoom: '重置',
-            resetZoomTitle: '重置缩放比例'
-        }
-	});
-	$('#body2').highcharts({
-		    chart: {
-	            panning: true,
-	            panKey: 'ctrl',
-		    	zoomType: 'x',
-		        type: 'spline',
-		        animation: false, // don't animate in old IE
-		        marginRight: 70,
-		    },
-		    plotOptions: {
-                line: {
-                    dataLabels: {
-                        enabled: true
-                    },
-                    enableMouseTracking: false
+    var myChart = echarts.init(document.getElementById('body2'));
+    var option = {
+        title : {
+            text : '电压'
+        },
+        tooltip : {
+            trigger : 'axis',
+        },
+        toolbox : {
+            show : true,
+            feature : {
+                mark : {
+                    show : false
                 },
-		    	series:{
-		    		turboThreshold:0
-		    	}
-            },
-		    title: {
-		        text: false
-		    },
-	        mapNavigation: {
-	            enabled: true,
-	            enableButtons: false
-	        },
-		    xAxis: {
-		        type: 'datetime',
-		        tickPixelInterval: 150
-		    },
-		    yAxis: [{
-            max:150, // 定义Y轴 最大值  
-            min:0, // 定义最小值  
-            minPadding: 0.2,   
-            maxPadding: 0.2,  
-            tickInterval:30,
-            color:'#87CEFA',
-		    	title: {
-		            text: '电压',
-	                style: {  
-	                    color: '#87CEFA'  
-	                }  
-		        },
-		    }],
-		    tooltip: {
-		        formatter: function () {
-		            return '<b>' + this.series.name + '</b><br/>' +
-		                Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
-		                Highcharts.numberFormat(this.y, 2);
-		        },
-		    },
-		    legend: {
-		        enabled: false
-		    },
-		    exporting: {
-		        enabled: false
-		    },
-		    credits:{
-		      enabled:false // 禁用版权信息
-		    },
-		    series: [{
-		        name: '电压',
-		        data: (function () {
-		            var data = [];
-		            for (i = 0; i < time1.length; i += 1) {
-		                data.push({
-		                    x: time1[i],
-		                    y: vol[i]
-		                });
-		            }
-		            return data;
-		        }()),
-		      
-		    }]
-		});
+                dataView : {
+                    show : false,
+                    readOnly : false
+                },
+                restore : {
+                    show : false
+                }
+            }
+        },
+        dataZoom : {//缩放
+            show : true,
+            start : 0
+        },
+        grid : {
+            y2 : $("#body2").height()//图表高度
+        },
+        xAxis : [ {
+			type:'category',
+			data: time1
+        } ],
+        yAxis : [ {
+            type : 'value',
+            max : 150,
+            min : 0
+        } ],
+        series : [ {
+            symbolSize : 5,//气泡大小
+      		name : '电压',
+      		type :'line',//折线图
+      		data : vol,
+      		itemStyle: {
+      	        normal: {
+      	            color: "#87CEFA",
+      	            lineStyle: {
+      	                color: "#87CEFA"
+      	            }
+      	        }
+      	    },
+        } ]
+    };
+    myChart.setOption(option);
+}
+
+//监听窗口大小变化
+window.onresize = function() {
+	setTimeout(domresize, 500);
+}
+
+//改变表格高宽
+function domresize() {
+	$("#dg").datagrid('resize', {
+		height : $("#dgtb").height()/2,
+		width : $("#dgtb").width()
+	});
 }
